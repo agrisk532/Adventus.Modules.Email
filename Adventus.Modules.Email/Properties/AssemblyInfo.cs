6c3f1a5f1859161482184a6ac455c07099992519
Agris Klimkans <agris.klimkans@adventus.lv>
2017-04-07 16:14:03 +0300

    AssemblyInfo fix

--- Adventus.Modules.Email/Properties/AssemblyInfo.cs
+++ Adventus.Modules.Email/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.1.3")]
-[assembly: AssemblyInformationalVersion("RC5")]
+[assembly: AssemblyInformationalVersion("RC5.0")]
