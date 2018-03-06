2c95a1089171cc5664ce39505986bd35b71ca9dc
Agris Klimkans <agris.klimkans@adventus.lv>
2018-03-06 13:58:34 +0200

    InteractionWorkspaceHelper.exe initial version

--- Adventus.Modules.Email/SaveAttachmentsCommand.cs
+++ Adventus.Modules.Email/SaveAttachmentsCommand.cs
@@ -22,6 +22,8 @@ using System.Net.Mail;
 using Genesyslab.Desktop.Modules.Windows.Views.Common.AttachmentView;
 using Genesyslab.Enterprise.Model.Contact;
 using Genesyslab.Desktop.Modules.Windows;
+//using System.IO.MemoryMappedFiles;
+//using InteractionWorkspaceHelper;
 
 namespace Adventus.Modules.Email
 {
@@ -145,6 +147,34 @@ namespace Adventus.Modules.Email
 					ShowAndLogErrorMsg("Type error. Email saving terminated.");
 					return true;
 				}
+
+                ////// BEGIN IPC test
+
+                //const int MMF_MAX_SIZE = 1024;  // allocated memory for this memory mapped file (bytes)
+                //const int MMF_VIEW_SIZE = 1024; // how many bytes of the allocated memory can this process access
+
+                //// creates the memory mapped file which allows 'Reading' and 'Writing'
+                //MemoryMappedFile mmf = MemoryMappedFile.CreateOrOpen("mmf1", MMF_MAX_SIZE, MemoryMappedFileAccess.ReadWrite);
+
+                //// creates a stream for this process, which allows it to write data from offset 0 to 1024 (whole memory)
+                //MemoryMappedViewStream mmvStream = mmf.CreateViewStream(0, MMF_VIEW_SIZE);
+
+                //// this is what we want to write to the memory mapped file
+                //FileWriter.Message message1 = new Message();
+                //message1.title = "test";
+                //message1.content = "hello world";
+
+                //// serialize the variable 'message1' and write it to the memory mapped file
+                //BinaryFormatter formatter = new BinaryFormatter();
+                //formatter.Serialize(mmvStream, message1);
+                //mmvStream.Seek(0, SeekOrigin.Begin); // sets the current position back to the beginning of the stream
+
+                //// the memory mapped file lives as long as this process is running
+                ////while (true) ;
+
+                ////// END IPC test
+
+
 
                 // get subjectLength
                 subjectLength = GetSubjectLength();
---
+++ InteractionWorkspaceHelper/Program.cs
@@ -0,0 +1,122 @@
+﻿using Genesyslab.Platform.Contacts.Protocols;
+using Genesyslab.Platform.Commons.Protocols;
+using System;
+using Genesyslab.Platform.Contacts.Protocols.ContactServer.Requests;
+using Genesyslab.Platform.Contacts.Protocols.ContactServer;
+using Genesyslab.Platform.Contacts.Protocols.ContactServer.Events;
+
+namespace InteractionWorkspaceHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const int MMF_MAX_SIZE = 1024;  // allocated memory for this memory mapped file (bytes)
+            //const int MMF_VIEW_SIZE = 1024; // how many bytes of the allocated memory can this process access
+
+            // creates the memory mapped file
+            //MemoryMappedFile mmf = MemoryMappedFile.OpenExisting("mmf1");
+            //MemoryMappedViewStream mmvStream = mmf.CreateViewStream(0, MMF_VIEW_SIZE); // stream used to read data
+
+            //BinaryFormatter formatter = new BinaryFormatter();
+
+            //// needed for deserialization
+            //byte[] buffer = new byte[MMF_VIEW_SIZE];
+
+            //Message message1;
+
+            //// reads every second what's in the shared memory
+            //while (mmvStream.CanRead)
+            //{
+            //    // stores everything into this buffer
+            //    mmvStream.Read(buffer, 0, MMF_VIEW_SIZE);
+
+            //    // deserializes the buffer & prints the message
+            //    message1 = (Message)formatter.Deserialize(new MemoryStream(buffer));
+            //    Console.WriteLine(message1.title + "\n" + message1.content + "\n");
+
+            //    System.Threading.Thread.Sleep(1000);
+            //}
+
+            // connection to UCS contact server
+            UniversalContactServerProtocol ucsConnection;
+            //ucsConnection = new UniversalContactServerProtocol(new Endpoint("UniversalContactServer", "ling.lauteri.inter", 5130));
+            ucsConnection = new UniversalContactServerProtocol(new Endpoint("eS_UniversalContactServer", "genesys1", 6120));
+            //ucsConnection = new UniversalContactServerProtocol(new Endpoint(appName, host, port));
+
+            //ucsConnection.Opened += new EventHandler(ucsConnection_Opened);
+            //ucsConnection.Error += new EventHandler(ucsConnection_Error);
+            //ucsConnection.Closed += new EventHandler(ucsConnection_Closed);
+            try
+            {
+                ucsConnection.Open();
+            }
+            catch (Exception e)
+            {
+                //ShowAndLogErrorMsg(String.Format("Connection to UniversalContactServer failed. Email saving terminated: {0}", e.ToString()));
+                //ucsConnection.Opened -= new EventHandler(ucsConnection_Opened);
+                //ucsConnection.Error -= new EventHandler(ucsConnection_Error);
+                //ucsConnection.Closed -= new EventHandler(ucsConnection_Closed);
+                //return true;    // stop execution of command chain
+            }
+
+            RequestGetInteractionContent request = new RequestGetInteractionContent();
+            //request.InteractionId = enterpriseEmailInteraction.Id;
+            request.InteractionId = "000D0aD8NBBM001Q";
+            request.IncludeBinaryContent = true;
+            request.IncludeAttachments = true;
+            //request.DataSource = new NullableDataSourceType(Model.Dst);
+            request.DataSource = new NullableDataSourceType(Genesyslab.Platform.Contacts.Protocols.ContactServer.DataSourceType.Main);
+
+            //GC.Collect(); // to avoid outofmemory exceptions
+            EventGetInteractionContent eventGetIxnContent = (EventGetInteractionContent)ucsConnection.Request(request);
+
+            Program pr = new Program();
+
+            if (eventGetIxnContent == null)
+            {
+                //ShowAndLogErrorMsg("Request to UniversalContactServer failed. Email saving terminated.");
+                //pr.CloseUCSConnection(ucsConnection);
+                //return true;    // stop execution of the command chain
+            }
+
+            AttachmentList attachmentList = eventGetIxnContent.Attachments;
+            InteractionContent interactionContent = eventGetIxnContent.InteractionContent;
+            pr.CloseUCSConnection(ucsConnection);
+            Environment.Exit(0);
+        }
+
+        private void CloseUCSConnection(UniversalContactServerProtocol ucsConnection)
+        {
+            if (ucsConnection.State != ChannelState.Closed && ucsConnection.State != ChannelState.Closing)
+            {
+                //ucsConnection.Opened -= new EventHandler(ucsConnection_Opened);
+                //ucsConnection.Error -= new EventHandler(ucsConnection_Error);
+                //ucsConnection.Closed -= new EventHandler(ucsConnection_Closed);
+                ucsConnection.Close();
+                ucsConnection.Dispose();
+            }
+        }
+
+
+        //private void ShowAndLogErrorMsg(string s)
+        //{
+        //    MessageBox.Show(METHOD_NAME + s, "Attention");
+        //    log.Error(METHOD_NAME + s);
+        //}
+
+        //private void ShowAndLogInfoMsg(string s)
+        //{
+        //    MessageBox.Show(METHOD_NAME + s, "Attention");
+        //    log.Info(METHOD_NAME + s);
+        //}
+    }
+
+
+    [Serializable]
+    class Message
+    {
+        public string title;
+        public string content;
+    }
+}
